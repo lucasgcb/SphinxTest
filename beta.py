18f7ec764c5b1c24e70f39cd98a7e0abc5d8b041
lucasgcb <lucascoliado@hotmail.com>
2019-02-22 13:43:37 +0100

    Add new files

--- beta.py
+++ beta.py
@@ -1,24 +1,40 @@
 from _errors import _ERR_NOTLIST, _ERR_NOT3D
 
 class Waldo:
-    """This is the Waldo Docstring
+    """This is the Waldo class
+        
+        Parameters
+        ----------
+        pos :  (Tuple(int,int,int)): 
+            Waldo's current position
+        
+        Arguments
+        ---------
+        pos :  (Tuple(int,int,int)): 
+            Start Waldo in this position
 
-        Args:
-            pos (Tuple(int,int,int)): Initialize Waldo in this position 
-
-        Example:
-            >>> Waldo([2,3,5])
-            >>> Waldo.whereIsHe()
-            >>> [2,3,5]
     """
 
     def __init__(self, pos):
+        
+
         assert type(pos) == list, _ERR_NOTLIST
         assert len(pos) == 3, _ERR_NOT3D
         self.pos=pos
 
     def whereIsHe(self):
-        """Show Waldo's Position"""
+        """Show Waldo's Position
+        Returns
+        -------
+        list
+            3 dimension coordinate of Waldo's position
+            
+        Example
+        -------
+        >>> Waldo([2,3,5])
+        >>> Waldo.whereIsHe()
+        >>> [2,3,5]
+        """
 
         print(self.pos)
     
